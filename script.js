7973e84c3353a3d7aa8dd84fc294c395f87e0f68
AndrePacolla <andreluizzpacolla@outlook.com>
2024-06-25 18:56:57 -0300

    create function open and close Modal

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const menu = document.getElementById('menu');
+const address = document.getElementById('address');
 const cartBtn = document.getElementById('cart-btn');
 const cartModal = document.getElementById('cart-modal');
 const cartItems = document.getElementById('cart-items');
 const cartTotal = document.getElementById('cart-total');
-const checkoutBtn = document.getElementById('checkout-btn');
-const closeBtn = document.getElementById('close-modal-btn-btn');
 const cartCounter = document.getElementById('card-count');
+const checkoutBtn = document.getElementById('checkout-btn');
+const addressWarn = document.getElementById('address-alert');
+const closeModalBtn = document.getElementById('close-modal-btn');
 
 
 
+cartBtn.addEventListener('click', ()=>{
+    cartModal.style.display = 'flex'
+});
 
+closeModalBtn.addEventListener('click', ()=>{
+    cartModal.style.display = 'none'
+});
+
+cartModal.addEventListener('click',(event)=>{
+
+    if(event.target === cartModal){
+        cartModal.style.display = 'none'
+    }
+});
 
