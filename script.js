d82da2e04c2cbb66bfbfc6b6219211058cafb577
AndrePacolla <andreluizzpacolla@outlook.com>
2024-06-25 18:32:00 -0300

    criando var correspondente aos elementos html  arvore DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const cartBtn = document.getElementById('cart-btn');
+const cartModal = document.getElementById('cart-modal');
+const cartItems = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+const checkoutBtn = document.getElementById('checkout-btn');
+const closeBtn = document.getElementById('close-modal-btn-btn');
+const cartCounter = document.getElementById('card-count');
+
+
+
+
+
